33e13c7c0be4559380dad61c2f38cf47e1e2adc7
Md Imam Ahasan <emamahasane@gmail.com>
2021-11-04 21:14:18 +0600

    Add files via upload

---
+++ Stack/stack.py
@@ -0,0 +1,31 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    
+    def push(self, items):
+        self.items.append(items)
+
+
+    def pop(self):
+        return self.items.pop()
+
+
+    def is_empty(self):
+        if self.items == []:
+            return True
+        return False
+
+
+if  __name__ == "__main__":
+    s = Stack()
+    s.push(1)
+    s.push(2)
+    s.push(3)
+    s.pop
+
+"""
+    while not s.is_empty():
+        item = s.pop()
+        print(item)
+
