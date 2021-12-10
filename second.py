aa89bc4332aff6c7db2cde6961bd8fc2c60c28ca
Md Imam Ahasan <emamahasane@gmail.com>
2021-12-11 01:03:49 +0600

    Add files via upload

---
+++ second.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  3 03:35:03 2019
+
+@author: emama
+"""
+
+n = input()
+n = int(n)
+
+result = n * (n+1) / 2
+print(result)
---
+++ th.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  3 03:38:51 2019
+
+@author: emama
+"""
+
+n = int(input())
+
+result = 0
+
+for i in range(1, n+1):
+    result += i
+print(result)
